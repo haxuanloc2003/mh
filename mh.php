275c006485e4e660342c304a276682c1a76e2427
haxuanloc2003 <127503743+haxuanloc2003@users.noreply.github.com>
2023-07-03 07:53:37 +0700

    mh.php

--- mh.php
+++ mh.php
@@ -1,8 +1,32 @@
-+<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $password = $_POST["password"];
+<?php
+// Thông tin kết nối đến cơ sở dữ liệu
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database";
 
-    // Mã hóa mật khẩu bằng thuật toán băm, ví dụ: bcrypt
-    $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+// Tạo kết nối đến cơ sở dữ liệu
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Kết nối đến cơ sở dữ liệu thất bại: " . $conn->connect_error);
 }
+
+// Dữ liệu đầu vào từ form
+$newUsername = $_POST['username'];
+$newPassword = $_POST['password'];
+
+// Mã hóa mật khẩu
+$hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+
+// Tạo câu truy vấn để thêm tài khoản mới vào cơ sở dữ liệu
+$sql = "INSERT INTO users (username, password) VALUES ('$newUsername', '$hashedPassword')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Tạo tài khoản thành công!";
+} else {
+    echo "Lỗi: " . $sql . "<br>" . $conn->error;
+}
+
+// Đóng kết nối đến cơ sở dữ liệu
+$conn->close();
 ?>
